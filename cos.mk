b4dd3d0b7ecba752a8395d6787322e5bd40ad1b2
Sagar Shah <sagarshah2326@gmail.com>
2018-01-06 22:35:46 +0530

    Cosmic-ify
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- cos.mk
+++ cos.mk
@@ -18,15 +18,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from oneplus2 device
-TARGET_ARCH := arm64
-TARGET_DENSITY := xxxhdpi
-TARGET_BOOT_ANIMATION_RES := 1080
 $(call inherit-product, device/oneplus/oneplus2/device.mk)
 
-# Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/cos/common.mk)
 
-PRODUCT_NAME := aosp_oneplus2
+PRODUCT_NAME := cos_oneplus2
 PRODUCT_DEVICE := oneplus2
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
